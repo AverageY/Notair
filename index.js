c5c47ee57f894d4c2c1e9b2c261597af6ff98707
AverageY <f20212330@pilani.bits-pilani.ac.in>
2024-01-02 00:21:23 +0530

    hotfix

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+  }
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
